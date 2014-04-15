1de6855d275e145534024ab30b51b3ab7cbbda58
Yamel Senih <Yamel Senih@localhost>
2014-04-15 12:05:07 +0200

    Change Model Package Class Name

---
+++ base/org/spinsuite/model/X_AD_Ref_List.java
@@ -0,0 +1,232 @@
+/******************************************************************************
+ * Product: Adempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 1999-2007 ComPiere, Inc. All Rights Reserved.                *
+ * This program is free software, you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program, if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA        *
+ * or via info@compiere.org or http://www.compiere.org/license.html           *
+ *****************************************************************************/
+/** Generated Model - DO NOT CHANGE */
+package org.spinsuite.model;
+
+import android.content.Context;
+import android.database.Cursor;
+import java.sql.Timestamp;
+import java.util.Date;
+import org.spinsuite.base.DB;
+import org.spinsuite.util.KeyNamePair;
+
+/** Generated Model for AD_Ref_List
+ *  @author Adempiere (generated) 
+ *  @version Release 3.7.0LTS (1252452765) - $Id$ */
+public class X_AD_Ref_List extends PO implements I_AD_Ref_List
+{
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 20140415L;
+
+    /** Standard Constructor */
+    public X_AD_Ref_List (Context ctx, int AD_Ref_List_ID, DB conn)
+    {
+      super (ctx, AD_Ref_List_ID, conn);
+      /** if (AD_Ref_List_ID == 0)
+        {
+			setAD_Reference_ID (0);
+			setAD_Ref_List_ID (0);
+			setEntityType (null);
+// U
+			setName (null);
+			setValue (null);
+        } */
+    }
+
+    /** Load Constructor */
+    public X_AD_Ref_List (Context ctx, Cursor rs, DB conn)
+    {
+      super (ctx, rs, conn);
+    }
+
+
+    /** Load Meta Data */
+    protected POInfo initPO (Context ctx)
+    {
+      POInfo poi = POInfo.getPOInfo (ctx, SPS_Table_ID, get_Connection());
+      return poi;
+    }
+
+    public String toString()
+    {
+      StringBuffer sb = new StringBuffer ("X_AD_Ref_List[")
+        .append(get_ID()).append("]");
+      return sb.toString();
+    }
+
+	/** Set Reference.
+		@param AD_Reference_ID 
+		System Reference and Validation
+	  */
+	public void setAD_Reference_ID (int AD_Reference_ID)
+	{
+		if (AD_Reference_ID < 1) 
+			set_Value (COLUMNNAME_AD_Reference_ID, null);
+		else 
+			set_Value (COLUMNNAME_AD_Reference_ID, Integer.valueOf(AD_Reference_ID));
+	}
+
+	/** Get Reference.
+		@return System Reference and Validation
+	  */
+	public int getAD_Reference_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_AD_Reference_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	/** Set Reference List.
+		@param AD_Ref_List_ID 
+		Reference List based on Table
+	  */
+	public void setAD_Ref_List_ID (int AD_Ref_List_ID)
+	{
+		if (AD_Ref_List_ID < 1) 
+			set_Value (COLUMNNAME_AD_Ref_List_ID, null);
+		else 
+			set_Value (COLUMNNAME_AD_Ref_List_ID, Integer.valueOf(AD_Ref_List_ID));
+	}
+
+	/** Get Reference List.
+		@return Reference List based on Table
+	  */
+	public int getAD_Ref_List_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_AD_Ref_List_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	/** Set Description.
+		@param Description 
+		Optional short description of the record
+	  */
+	public void setDescription (String Description)
+	{
+		set_Value (COLUMNNAME_Description, Description);
+	}
+
+	/** Get Description.
+		@return Optional short description of the record
+	  */
+	public String getDescription () 
+	{
+		return (String)get_Value(COLUMNNAME_Description);
+	}
+
+	/** EntityType AD_Reference_ID=389 */
+	public static final int ENTITYTYPE_AD_Reference_ID=389;
+	/** Set Entity Type.
+		@param EntityType 
+		Dictionary Entity Type; Determines ownership and synchronization
+	  */
+	public void setEntityType (String EntityType)
+	{
+
+		set_Value (COLUMNNAME_EntityType, EntityType);
+	}
+
+	/** Get Entity Type.
+		@return Dictionary Entity Type; Determines ownership and synchronization
+	  */
+	public String getEntityType () 
+	{
+		return (String)get_Value(COLUMNNAME_EntityType);
+	}
+
+	/** Set Name.
+		@param Name 
+		Alphanumeric identifier of the entity
+	  */
+	public void setName (String Name)
+	{
+		set_Value (COLUMNNAME_Name, Name);
+	}
+
+	/** Get Name.
+		@return Alphanumeric identifier of the entity
+	  */
+	public String getName () 
+	{
+		return (String)get_Value(COLUMNNAME_Name);
+	}
+
+    /** Get Record ID/ColumnName
+        @return ID/ColumnName pair
+      */
+    public KeyNamePair getKeyNamePair() 
+    {
+        return new KeyNamePair(get_ID(), getName());
+    }
+
+	/** Set Valid from.
+		@param ValidFrom 
+		Valid from including this date (first day)
+	  */
+	public void setValidFrom (Date ValidFrom)
+	{
+		set_Value (COLUMNNAME_ValidFrom, ValidFrom);
+	}
+
+	/** Get Valid from.
+		@return Valid from including this date (first day)
+	  */
+	public Date getValidFrom () 
+	{
+		return (Date)get_Value(COLUMNNAME_ValidFrom);
+	}
+
+	/** Set Valid to.
+		@param ValidTo 
+		Valid to including this date (last day)
+	  */
+	public void setValidTo (Date ValidTo)
+	{
+		set_Value (COLUMNNAME_ValidTo, ValidTo);
+	}
+
+	/** Get Valid to.
+		@return Valid to including this date (last day)
+	  */
+	public Date getValidTo () 
+	{
+		return (Date)get_Value(COLUMNNAME_ValidTo);
+	}
+
+	/** Set Search Key.
+		@param Value 
+		Search key for the record in the format required - must be unique
+	  */
+	public void setValue (String Value)
+	{
+		set_Value (COLUMNNAME_Value, Value);
+	}
+
+	/** Get Search Key.
+		@return Search key for the record in the format required - must be unique
+	  */
+	public String getValue () 
+	{
+		return (String)get_Value(COLUMNNAME_Value);
+	}
+}
---
+++ base/org/spinsuite/model/X_AD_User_OrgAccess.java
@@ -0,0 +1,115 @@
+/******************************************************************************
+ * Product: Adempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 1999-2007 ComPiere, Inc. All Rights Reserved.                *
+ * This program is free software, you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program, if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA        *
+ * or via info@compiere.org or http://www.compiere.org/license.html           *
+ *****************************************************************************/
+/** Generated Model - DO NOT CHANGE */
+package org.spinsuite.model;
+
+import android.content.Context;
+import android.database.Cursor;
+import java.util.Date;
+import org.spinsuite.base.DB;
+
+/** Generated Model for AD_User_OrgAccess
+ *  @author Adempiere (generated) 
+ *  @version Release 3.7.0LTS (1252452765) - $Id$ */
+public class X_AD_User_OrgAccess extends PO implements I_AD_User_OrgAccess
+{
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 20140415L;
+
+    /** Standard Constructor */
+    public X_AD_User_OrgAccess (Context ctx, int AD_User_OrgAccess_ID, DB conn)
+    {
+      super (ctx, AD_User_OrgAccess_ID, conn);
+      /** if (AD_User_OrgAccess_ID == 0)
+        {
+			setAD_User_ID (0);
+			setIsReadOnly (false);
+// N
+        } */
+    }
+
+    /** Load Constructor */
+    public X_AD_User_OrgAccess (Context ctx, Cursor rs, DB conn)
+    {
+      super (ctx, rs, conn);
+    }
+
+
+    /** Load Meta Data */
+    protected POInfo initPO (Context ctx)
+    {
+      POInfo poi = POInfo.getPOInfo (ctx, SPS_Table_ID, get_Connection());
+      return poi;
+    }
+
+    public String toString()
+    {
+      StringBuffer sb = new StringBuffer ("X_AD_User_OrgAccess[")
+        .append(get_ID()).append("]");
+      return sb.toString();
+    }
+
+	/** Set Usuario.
+		@param AD_User_ID 
+		User within the system - Internal or Business Partner Contact
+	  */
+	public void setAD_User_ID (int AD_User_ID)
+	{
+		if (AD_User_ID < 1) 
+			set_Value (COLUMNNAME_AD_User_ID, null);
+		else 
+			set_Value (COLUMNNAME_AD_User_ID, Integer.valueOf(AD_User_ID));
+	}
+
+	/** Get Usuario.
+		@return User within the system - Internal or Business Partner Contact
+	  */
+	public int getAD_User_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_AD_User_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	/** Set Read Only.
+		@param IsReadOnly 
+		Field is read only
+	  */
+	public void setIsReadOnly (boolean IsReadOnly)
+	{
+		set_Value (COLUMNNAME_IsReadOnly, Boolean.valueOf(IsReadOnly));
+	}
+
+	/** Get Read Only.
+		@return Field is read only
+	  */
+	public boolean isReadOnly () 
+	{
+		Object oo = get_Value(COLUMNNAME_IsReadOnly);
+		if (oo != null) 
+		{
+			 if (oo instanceof Boolean) 
+				 return ((Boolean)oo).booleanValue(); 
+			return "Y".equals(oo);
+		}
+		return false;
+	}
+}
