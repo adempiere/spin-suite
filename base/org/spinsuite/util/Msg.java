2194f8754c9efa7fbdb09aa0a37a3e53f239ba7c
Yamel Senih <Yamel Senih@localhost>
2014-08-29 22:00:37 +0200

    Add Msg Support..... missing development

--- base/org/spinsuite/util/Msg.java
+++ base/org/spinsuite/util/Msg.java
@@ -21,6 +21,7 @@ import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -129,169 +130,330 @@ public class Msg {
 		toast.show();
 	}
 
-	/**
-	 * Lanza un mensaje en la barra de notificaciones de android y vuelve a actividad actual
-	 * @author Carlos Parada 03/11/2012, 01:53:16
-	 * @param ctx
-	 * @param icon
-	 * @param systemMsg
-	 * @param tittleMsg
-	 * @param bodyMsg
-	 * @param notificationID
-	 * @param activityReturn
-
-	 * @return
-	 * @return NotificationManager
+	/**************************************************************************
+	 *	Get translated text for AD_Message
+	 *  @param  ad_language - Language
+	 *  @param	AD_Message - Message Key
+	 *  @return translated text
 	 */
-	/*public static NotificationManager notificationMsg(Context ctx,int icon,String systemMsg,String tittleMsg,String bodyMsg,int notificationID,Intent currentIntent)
+	/*public static String getMsg (Context ctx, String ad_language, String AD_Message)
 	{
-		return notificationMsg(ctx,icon,systemMsg,tittleMsg,bodyMsg,notificationID,null,currentIntent);
-	}*/
-	/**
-	 * Lanza un mensaje en la barra de notificaciones de android y vuelve a actividad nueva
-	 * @author Carlos Parada 03/11/2012, 01:53:16
-	 * @param ctx
-	 * @param icon
-	 * @param systemMsg
-	 * @param tittleMsg
-	 * @param bodyMsg
-	 * @param notificationID
-	 * @param activityReturn
-
-	 * @return
-	 * @return NotificationManager
-	 */
-	/*public static NotificationManager notificationMsg(Context ctx,int icon,String systemMsg,String tittleMsg,String bodyMsg,int notificationID,Class activityReturn)
-	{
-		return notificationMsg(ctx,icon,systemMsg,tittleMsg,bodyMsg,notificationID,activityReturn,null);
-	}*/
-	/**
-	 * Lanza un mensaje en la barra de notificaciones de android
-	 * @author Carlos Parada 05/08/2012, 08:55:06
-	 * @param ctx
-	 * @param icon
-	 * @param systemMsg
-	 * @param tittleMsg
-	 * @param bodyMsg
-	 * @param notificationID
-	 * @param activityReturn
-	 * @param currentIntent
-	 * @return
-	 * @return NotificationManager
-	 */
-	/*public static NotificationManager notificationMsg(Context ctx,int icon,String systemMsg,String tittleMsg,String bodyMsg,int notificationID,Class activityReturn,Intent currentIntent)
-	{
-		PendingIntent contIntent=null ;
-		Msg.notification_ID=notificationID;
-		String ns = Context.NOTIFICATION_SERVICE;
-		NotificationManager notManager = (NotificationManager) ctx.getSystemService(ns);
-		 
-		Msg.notification= new Notification(icon, systemMsg, System.currentTimeMillis());
-		
-		if (activityReturn!=null)
-		{	
-		Intent notIntent = new Intent(ctx,activityReturn);
-		
-		contIntent = PendingIntent.getActivity(
-				ctx, 0, notIntent, PendingIntent.FLAG_ONE_SHOT);
+		if (AD_Message == null || AD_Message.length() == 0)
+			return "";
+		//
+		String AD_Language = ad_language;
+		if (AD_Language == null || AD_Language.length() == 0)
+			AD_Language = Env.BASE_LANGUAGE;
+		//	Get Message
+		String retStr = null;//get().lookup (AD_Language, AD_Message);
+		//
+		if (retStr == null || retStr.length() == 0)
+		{
+			LogM.log(ctx, "Msg", Level.WARNING, "NOT found: " + AD_Message);
+			return AD_Message;
 		}
-		else if (currentIntent!=null)
-			contIntent = PendingIntent.getActivity(
-					ctx, 0, currentIntent, PendingIntent.FLAG_ONE_SHOT);
-		
-		
-		Msg.notification.setLatestEventInfo(ctx, tittleMsg, bodyMsg, contIntent);
-		//Borra el mensaje de notificacion al seleccionarlo 
-		Msg.notification.flags|=Msg.notification.FLAG_AUTO_CANCEL;
-		//Iluminacion del Telefono
-		Msg.notification.flags|=Msg.notification.DEFAULT_LIGHTS;
-		//Vibracion al Momento de lanzar la notificación
-		Msg.notification.flags|=Msg.notification.DEFAULT_VIBRATE;
-		
-		notManager.notify(Msg.notification_ID, Msg.notification);
-		
-		return notManager;
-	}*/
-	/**
-	 * Lanza un mensaje en la barra de notificacion mediante una vista personalizada y retorna a una ventana nueva
-	 * @author Carlos Parada 03/11/2012, 03:24:43
-	 * @param ctx
-	 * @param icon
-	 * @param systemMsg
-	 * @param notificationID
-	 * @param activityReturn
-	 * @param contentView
-	 * @return
-	 * @return NotificationManager
-	 */
-	/*public static NotificationManager notificationMsg(Context ctx,int icon,String systemMsg,int notificationID,Class activityReturn,RemoteViews contentView)
-	{
-		return notificationMsg(ctx,icon,systemMsg,notificationID,activityReturn,null,contentView);
-	}*/
-	/**
-	 * Lanza un mensaje en la barra de notificacion mediante una vista personalizada y retorna a la ventana actual
-	 * @author Carlos Parada 03/11/2012, 03:28:33
-	 * @param ctx
-	 * @param icon
-	 * @param systemMsg
-	 * @param notificationID
-	 * @param currentIntent
-	 * @param contentView
-	 * @return
-	 * @return NotificationManager
-	 */
-	/*public static NotificationManager notificationMsg(Context ctx,int icon,String systemMsg,int notificationID,Intent currentIntent,RemoteViews contentView)
-	{
-		return notificationMsg(ctx,icon,systemMsg,notificationID,null,currentIntent,contentView);
-	}*/
-	/**
-	 * Lanza un mensaje en la barra de notificacion mediante una vista personalizada
-	 * @author Carlos Parada 05/08/2012, 18:55:37
-	 * @param ctx
-	 * @param icon
-	 * @param systemMsg
-	 * @param notificationID
-	 * @param activityReturn
-	 * @param contentView
-	 * @return
-	 * @return NotificationManager
-	 */
-	/*public static NotificationManager notificationMsg(Context ctx,int icon,String systemMsg,int notificationID,Class activityReturn,Intent currentIntent,RemoteViews contentView)
-	{
-		Intent notIntent =null;
-		Msg.notification_ID=notificationID;
-		Msg.contentView=contentView;
-		String ns = Context.NOTIFICATION_SERVICE;
-		NotificationManager notManager = (NotificationManager) ctx.getSystemService(ns);
-		 
-		Msg.notification= new Notification(icon, systemMsg, System.currentTimeMillis());
-		Msg.notification.contentView=Msg.contentView;
-		if (activityReturn!=null)
-			notIntent = new Intent(ctx,activityReturn);
-		else
-			notIntent=currentIntent;
-		 
-		PendingIntent contIntent = PendingIntent.getActivity(ctx, 0, notIntent, PendingIntent.FLAG_ONE_SHOT);
-		Msg.notification.contentIntent = contIntent;
-		
-		//Borra el mensaje de notificacion al seleccionarlo 
-		//Msg.notification.flags|=Msg.notification.FLAG_AUTO_CANCEL;
-		//Iluminacion del Telefono
-		Msg.notification.flags|=Msg.notification.DEFAULT_LIGHTS;
-		//Vibracion al Momento de lanzar la notificación
-		Msg.notification.flags|=Msg.notification.DEFAULT_VIBRATE;
-		notManager.notify(Msg.notification_ID, Msg.notification);
-		
-		return notManager;
-	}*/
 
+		return retStr;
+	}	//	getMsg*/
 
-	//Nro Identificador de la notificación
-	//public static int notification_ID;
-	//Objeto de Notificación 
-	//public static Notification notification;
-	//Vista remota de Notificacion
-	//public static RemoteViews contentView;
+	/**
+	 *  Get translated text message for AD_Message
+	 *  @param  ctx Context to retrieve language
+	 *  @param	AD_Message - Message Key
+	 *  @return translated text
+	 */
+	/*public static String getMsg (Context ctx, String AD_Message)
+	{
+		return getMsg (ctx, Env.getAD_Language(ctx), AD_Message);
+	}   //  getMeg*/
 	
+	/**
+	 *	Get clear text for AD_Message with parameters
+	 *  @param  ctx Context to retrieve language
+	 *  @param AD_Message   Message key
+	 *  @param args         MessageFormat arguments
+	 *  @return translated text
+	 *  @see java.text.MessageFormat for formatting options
+	 */
+	/*public static String getMsg(Context ctx, String AD_Message, Object[] args)
+	{
+		return getMsg (ctx, Env.getAD_Language(ctx), AD_Message, args);
+	}	//	getMsg*/
+
+	/**
+	 *	Get clear text for AD_Message with parameters
+	 *  @param ad_language  Language
+	 *  @param AD_Message   Message key
+	 *  @param args         MessageFormat arguments
+	 *  @return translated text
+	 *  @see java.text.MessageFormat for formatting options
+	 */
+	/*public static String getMsg (Context ctx, String ad_language, String AD_Message, Object[] args)
+	{
+		String msg = getMsg(ctx, ad_language, AD_Message);
+		String retStr = msg;
+		try
+		{
+			retStr = MessageFormat.format(msg, args);	//	format string
+		}
+		catch (Exception e)
+		{
+			LogM.log(ctx, "Msg", Level.SEVERE, "NOT found: " + AD_Message);
+		}
+		return retStr;
+	}	//	getMsg*/
+
+
+	/**************************************************************************
+	 * 	Get Amount in Words
+	 * 	@param language language
+	 * 	@param amount numeric amount (352.80)
+	 * 	@return amount in words (three*five*two 80/100)
+	 */
+	public static String getAmtInWords (String language, String amount) {
+		if (amount == null || language == null)
+			return amount;
+		//	Try to find Class
+		String className = "org.compiere.util.AmtInWords_";
+		try {
+			className += language.substring(3).toUpperCase();
+			Class<?> clazz = Class.forName(className);
+			AmtInWords aiw = (AmtInWords)clazz.newInstance();
+			return aiw.getAmtInWords(amount);
+		} catch (ClassNotFoundException e) {
+			Log.d("Msg", "Class not found: " + className);
+		} catch (Exception e) {
+			Log.e("Msg", "Class not found: " + className, e);
+		}
+		
+		//	Fallback
+		StringBuffer sb = new StringBuffer();
+		int pos = amount.lastIndexOf('.');
+		int pos2 = amount.lastIndexOf(',');
+		if (pos2 > pos)
+			pos = pos2;
+		for (int i = 0; i < amount.length(); i++)
+		{
+			if (pos == i)	//	we are done
+			{
+				String cents = amount.substring(i+1);
+				sb.append(' ').append(cents).append("/100");
+				break;
+			}
+			else
+			{
+				char c = amount.charAt(i);
+				if (c == ',' || c == '.')	//	skip thousand separator
+					continue;
+				if (sb.length() > 0)
+					sb.append("*");
+				sb.append(String.valueOf(c));
+			}
+		}
+		return sb.toString();
+	}	//	getAmtInWords
+
+
+	/**************************************************************************
+	 *  Get Translation for Element
+	 *  @param ad_language language
+	 *  @param ColumnName column name
+	 *  @param isSOTrx if false PO terminology is used (if exists)
+	 *  @return Name of the Column or "" if not found
+	 */
+	/*public static String getElement (Context ctx, String ad_language, String ColumnName, boolean isSOTrx)
+	{
+		if (ColumnName == null || ColumnName.equals(""))
+			return "";
+		String AD_Language = ad_language;
+		if (AD_Language == null || AD_Language.length() == 0)
+			AD_Language = Language.getBaseAD_Language();
+
+		//	Check AD_Element
+		String retStr = "";
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try
+		{
+			try
+			{
+				if (AD_Language == null || AD_Language.length() == 0 || Env.isBaseLanguage(AD_Language, "AD_Element"))
+					pstmt = DB.prepareStatement("SELECT Name, PO_Name FROM AD_Element WHERE UPPER(ColumnName)=?", null);
+				else
+				{
+					pstmt = DB.prepareStatement("SELECT t.Name, t.PO_Name FROM AD_Element_Trl t, AD_Element e "
+						+ "WHERE t.AD_Element_ID=e.AD_Element_ID AND UPPER(e.ColumnName)=? "
+						+ "AND t.AD_Language=?", null);
+					pstmt.setString(2, AD_Language);
+				}
+			}
+			catch (Exception e)
+			{
+				return ColumnName;
+			}
+			finally {
+				DB.close(rs);
+				rs = null;
+			}
+			pstmt.setString(1, ColumnName.toUpperCase());
+			rs = pstmt.executeQuery();
+			if (rs.next())
+			{
+				retStr = rs.getString(1);
+				if (!isSOTrx)
+				{
+					String temp = rs.getString(2);
+					if (temp != null && temp.length() > 0)
+						retStr = temp;
+				}
+			}
+		} catch (SQLException e) {
+			LogM.log(ctx, "Msg", Level.SEVERE, "getElement", e);
+			return "";
+		}
+		finally
+		{
+			DB.close(rs, pstmt);
+			rs = null; pstmt = null;
+		}
+		if (retStr != null)
+			return retStr.trim();
+		return retStr;
+	}   //  getElement*/
+
+	/**
+	 *  Get Translation for Element using Sales terminology
+	 *  @param ctx context
+	 *  @param ColumnName column name
+	 *  @return Name of the Column or "" if not found
+	 */
+	/*public static String getElement (Context ctx, String ColumnName)
+	{
+		return getElement (Env.getAD_Language(ctx), ColumnName, true);
+	}   //  getElement*/
+	
+	/**
+	 *  Get Translation for Element
+	 *  @param ctx context
+	 *  @param ColumnName column name
+	 *  @param isSOTrx sales transaction
+	 *  @return Name of the Column or "" if not found
+	 */
+	/*public static String getElement (Context ctx, String ColumnName, boolean isSOTrx)
+	{
+		return getElement (Env.getAD_Language(ctx), ColumnName, isSOTrx);
+	}   //  getElement*/
+
+
+	/**************************************************************************
+	 *	"Translate" text.
+	 *  <pre>
+	 *		- Check AD_Message.AD_Message 	->	MsgText
+	 *		- Check AD_Element.ColumnName	->	Name
+	 *  </pre>
+	 *  If checking AD_Element, the SO terminology is used.
+	 *  @param ad_language  Language
+	 *  @param isSOTrx sales order context
+	 *  @param text	Text - MsgText or Element Name
+	 *  @return translated text or original text if not found
+	 */
+	/*public static String translate(String ad_language, boolean isSOTrx, String text)
+	{
+		if (text == null || text.equals(""))
+			return "";
+		String AD_Language = ad_language;
+		if (AD_Language == null || AD_Language.length() == 0)
+			AD_Language = Env.BASE_LANGUAGE;
+
+		//	Check AD_Message
+		String retStr = null;//get().lookup (AD_Language, text);
+		if (retStr != null)
+			return retStr;
+
+		//	Check AD_Element
+		retStr = getElement(AD_Language, text, isSOTrx);
+		if (!retStr.equals(""))
+			return retStr.trim();
+
+		//	Nothing found
+		if (!text.startsWith("*"))
+			s_log.warning("NOT found: " + text);
+		return text;
+	}	//	translate*/
+
+	/***
+	 *	"Translate" text (SO Context).
+	 *  <pre>
+	 *		- Check AD_Message.AD_Message 	->	MsgText
+	 *		- Check AD_Element.ColumnName	->	Name
+	 *  </pre>
+	 *  If checking AD_Element, the SO terminology is used.
+	 *  @param ad_language  Language
+	 *  @param text	Text - MsgText or Element Name
+	 *  @return translated text or original text if not found
+	 */
+	/*public static String translate(String ad_language, String text)
+	{
+		return translate (ad_language, true, text);
+	}	//	translate*/
+
+	/**
+	 *	"Translate" text.
+	 *  <pre>
+	 *		- Check AD_Message.AD_Message 	->	MsgText
+	 *		- Check AD_Element.ColumnName	->	Name
+	 *  </pre>
+	 *  @param ctx  Context
+	 *  @param text	Text - MsgText or Element Name
+	 *  @return translated text or original text if not found
+	 */
+	/*public static String translate(Context ctx, String text)
+	{
+		if (text == null || text.length() == 0)
+			return text;
+		String s = (String)ctx.getProperty(text);
+		if (s != null && s.length() > 0)
+			return s;
+		return translate (Env.getAD_Language(ctx), Env.isSOTrx(ctx), text);
+	}   //  translate*/
+
+	/**
+	 *	Translate elements enclosed in "@" (at sign)
+	 *  @param ctx      Context
+	 *  @param text     Text
+	 *  @return translated text or original text if not found
+	 */
+	/*public static String parseTranslation(Context ctx, String text)
+	{
+		if (text == null || text.length() == 0)
+			return text;
+
+		String inStr = text;
+		String token;
+		StringBuffer outStr = new StringBuffer();
+
+		int i = inStr.indexOf('@');
+		while (i != -1)
+		{
+			outStr.append(inStr.substring(0, i));			// up to @
+			inStr = inStr.substring(i+1, inStr.length());	// from first @
+
+			int j = inStr.indexOf('@');						// next @
+			if (j < 0)										// no second tag
+			{
+				inStr = "@" + inStr;
+				break;
+			}
+
+			token = inStr.substring(0, j);
+			outStr.append(translate(ctx, token));			// replace context
+
+			inStr = inStr.substring(j+1, inStr.length());	// from second @
+			i = inStr.indexOf('@');
+		}
+
+		outStr.append(inStr);           					//	add remainder
+		return outStr.toString();
+	}   //  parseTranslation*/
 	
 }
